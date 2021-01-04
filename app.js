9d1b2e33d141aa0ef2c66d62b9c5bacbb457eb9f
FlorentLem <florentlempro@gmail.com>
2021-01-04 11:11:03 +0100

    yes

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+const myLogger = (req, res, next) => {
+    console.log("hello middleware");
+    next();
+};
+
+app.use(myLogger);
+
+app.get("/superMiddleware", (req, res) => {
+   res.send("hello world");
+});
+
+app.listen(3000, () => console.log(`Server up and running on port 3000 !`));
